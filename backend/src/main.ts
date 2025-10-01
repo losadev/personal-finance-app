3ad830f5baf8bb13a0544b68c20c4ca36cca65b8
losadev <losadauresdev@gmail.com>
2025-10-01 20:10:44 +0200

    initial commit

---
+++ backend/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001).then(() => console.log("Server running on http://localhost:3001"));
+}
+bootstrap();
