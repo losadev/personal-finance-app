a7fa2bf81a4d240119127bf000ee3a5a5aaeee9b
losadev <losadauresdev@gmail.com>
2025-10-02 10:15:03 +0200

    feat: input form refactor and react-hook-form validation

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Image from "next/image";
 import React from "react";
 import { Control, Controller, FieldError } from "react-hook-form";
 
@@ -5,11 +8,11 @@ interface Props {
   label: string;
   type: string;
   name: string;
-  control?: Control<any>
-  error?: FieldError
+  control?: Control<any>;
+  error?: FieldError;
 }
 
-const Input = ({label,name,type, control, error}: Props) => {
+const Input = ({ label, name, type, control, error }: Props) => {
   return (
     <fieldset className="flex flex-col gap-2">
       <label
@@ -18,8 +21,22 @@ const Input = ({label,name,type, control, error}: Props) => {
       >
         {label}
       </label>
-      <div className="border border-[#696868] px-5 py-2 rounded-lg">
-        <Controller name={name} control={control} render={({field}) => <input type={type} {...field} id={name} className="w-full" />}/>
+      <div className="flex w-full border border-gray-500  px-5 py-2 rounded-lg gap-2">
+        <Controller
+          name={name}
+          control={control}
+          render={({ field }) => (
+            <input type={type} {...field} id={name} className="w-full" />
+          )}
+        />
+        {type === "password" ? (
+          <Image
+            alt="icon show password"
+            src="/assets/images/icon-show-password.svg"
+            width={16}
+            height={16}
+          />
+        ) : null}
       </div>
       <span className="m-1.5">{error && error?.message}</span>
     </fieldset>
