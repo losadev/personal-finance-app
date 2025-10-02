1d7d288d7e5fa50b69558b262b1740251328637c
losadev <losadauresdev@gmail.com>
2025-10-02 09:28:34 +0200

    feat: custom input

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,23 +1,27 @@
 import React from "react";
+import { Control, Controller, FieldError } from "react-hook-form";
 
 interface Props {
   label: string;
   type: string;
   name: string;
+  control?: Control<any>
+  error?: FieldError
 }
 
-const Input = ({label,name,type}: Props) => {
+const Input = ({label,name,type, control, error}: Props) => {
   return (
     <fieldset className="flex flex-col gap-2">
       <label
-        htmlFor="email"
+        htmlFor={name}
         className="text-[12px] font-bold leading-1.5 tracking-normal text-[#696868]"
       >
-        Name
+        {label}
       </label>
       <div className="border border-[#696868] px-5 py-2 rounded-lg">
-        <input type="email" name="email" id="email" className="w-full" />
+        <Controller name={name} control={control} render={({field}) => <input type={type} {...field} id={name} className="w-full" />}/>
       </div>
+      <span className="m-1.5">{error && error?.message}</span>
     </fieldset>
   );
 };
