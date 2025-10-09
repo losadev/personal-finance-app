04e4963e62645ab06b15acb50f7fd4e87cd7407e
losadev <losadauresdev@gmail.com>
2025-10-09 11:52:24 +0200

    feat: authorization guards and token

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -12,12 +12,14 @@ const auth_module_1 = require("./auth/auth.module");
 const config_1 = require("@nestjs/config");
 const user_module_1 = require("./user/user.module");
 const prisma_module_1 = require("./prisma.module");
+const app_controller_1 = require("./app.controller");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [auth_module_1.AuthModule, config_1.ConfigModule.forRoot(), user_module_1.UserModule, prisma_module_1.PrismaModule],
+        controllers: [app_controller_1.AppController],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {cors: true });
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', // Reemplaza con el origen de tu frontend
+  //   credentials: true, // Permite enviar cookies, al poner esto en true, no puedo poner origin: '*'
+  // });
+  app.use(cookieParser()); // Middleware para parsear cookies
+  app.setGlobalPrefix('api'); // Prefijo global para todas las rutas /api/auth/signin
   await app
     .listen(process.env.PORT ?? 3001)
-    .then(() => console.log('Server running on http://localhost:3001'));
+    .then(() => console.log('Server running on http://localhost:3001/api'));
 }
 bootstrap();
