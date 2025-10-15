c2ef818ef1ac27c7f3a01a869debcc484d58d489
losadev <losadauresdev@gmail.com>
2025-10-15 17:44:03 +0200

    feat: filters and search

--- frontend/app/dashboard/transactions/page.tsx
+++ frontend/app/dashboard/transactions/page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import TransactionsCard from '@/components/ui/overview/TransactionsCard'
+import Image from 'next/image'
 
-type Props = {}
-
-function page({}: Props) {
+function page() {
   return (
-    <div>Transactions</div>
+    <main className='py-6 px-4 flex flex-col gap-8 bg-[var(--color-beige-100-app)] h-full'>
+       <h1 className=" text-[var(--color-gray-900-app)] text-preset-1">
+          Transactions
+        </h1>
+        {/* Table */}
+        <section className='bg-[var(--color-white-app)] h-full rounded-lg py-6 px-5 flex flex-col gap-6'>
+          <div className='flex flex-row justify-between w-full gap-2'>
+            <div className='px-5 py-3 rounded-lg flex  border-1 border-[var(--color-beige-500-app)] placeholder:text-preset-4'>
+              <input type="text" placeholder='Search transaction'/>
+              <Image alt='icon search' src={'/assets/images/icon-search.svg'} width={20} height={20}/>
+            </div>
+            <div className='flex gap-6'>
+              {/* Filters */}
+              <Image alt='sort' src={'/assets/images/sort.svg'} width={20} height={20}/>
+              <Image alt='filter' src={'/assets/images/icon-filter-mobile.svg'} width={20} height={20}/>
+            </div>
+          </div>
+          <div>
+          </div>
+        </section>
+    </main>
   )
 }
 
