f9aace4fded59df792b0221419fdc039163d2711
losadev <losadauresdev@gmail.com>
2026-05-04 21:32:11 +0200

    endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,71 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// TRANSACTIONS APIs
+
+Route::get('/transactions', function(Request $request) {
+    return 'hola';
+});
+
+// Budgets APIs
+
+Route::delete('/budgets/{id}', function(string $id) {
+    return 'Budget  deleted: ' .  $id;
+});
+
+
+Route::get('/budgets', function(Request $request) {
+    return 'Budgets';
+});
+
+
+Route::get('/budgets/{id}/transactions', function(Request $request, string $id) {
+    return 'Last 3 transactions: '. $id;
+});
+
+Route::patch('/budgets/{id}', function(Request $request, string $id) {
+    return 'Budget modificado: '. $id;
+});
+
+Route::post('/budgets', function(Request $request) {
+    return 'Budget creado';
+});
+
+
+// Pots APIs
+
+Route::delete('/pots/{id}', function(string $id) {
+    return 'Pots  deleted: ' .  $id;
+});
+
+
+Route::get('/pots', function(Request $request) {
+    return 'Pots';
+});
+
+Route::patch('/pots/{id}', function(Request $request, string $id) {
+    return 'Pot modificado: '. $request->id;
+});
+
+Route::post('/pots', function(Request $request) {
+    return 'Pot creado';
+});
+
+Route::post('/pots/{id}/money', function(Request $request, string $id) {
+    return 'Dinero añadido';
+});
+
+Route::post('/pots/{id}/withdraw', function(Request $request) {
+    return 'Dinero acado';
+});
+
+// Balances APIs
+
+Route::get('/balances/{id}', function(string $id) {
+    return 'Balance obtenido' . $id;
+});
+
+Route::get('/balances/{id}', function(Request $request) {
+    return 'Balance modificado: ' . $request->id;
+});
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function() {
+    return '<h1>Home</h1>';
+});
+
+Route::get('/transactions', function() {
+    return 'Transactions';
+});
+
+Route::get('/budgets', function() {
+    return '<h1>Budgets</h1>';
+});
+
+Route::get('/pots', function() {
+    return '<h1>Pots</h1>';
+});
+
+
+Route::get('/recurring-bills', function() {
+    return '<h1>Recurring bills</h1>';
+});
+
+
+Route::get('/sign-up', function() {
+    return '<h1>Sign Up</h1>';
+});
+
+
+
+Route::get('/login', function() {
+    return '<h1>Login</h1>';
+});
