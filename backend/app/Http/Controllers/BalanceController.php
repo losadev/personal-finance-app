596fa50ca7e8fb5ce071ad7011c8d5777498c3f0
losadev <losadauresdev@gmail.com>
2026-06-28 12:33:04 +0200

    core: uptade api deleted

--- backend/app/Http/Controllers/BalanceController.php
+++ backend/app/Http/Controllers/BalanceController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Balance;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class BalanceController extends Controller
@@ -15,16 +14,5 @@ class BalanceController extends Controller
     {
         $balance = Balance::findOrFail($id);
         return response()->json($balance, 200);
-    }
-
-    public function update(Request $request, string $id) {
-        $money = $request->money;
-
-        $currentBalance = Balance::findOrFail($id);
-
-        // devuelve el numero de rows afectadas
-        $x = $currentBalance->update(['income' => $money]);
-
-        return response()->json($x, 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,30 +11,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// TRANSACTIONS APIs
 Route::controller(TransactionController::class)->group(function() {
     Route::get('/transactions/{page?}/{name?}/{sort?}/{category?}','index')->name('transactions.index');
     Route::get('/transactions', 'getRecurringBills')->name('transactions.getRecurringBills');
 });
 
-// Budgets APIs
 Route::controller(BudgetController::class)->group(function() {
     Route::get('/budgets/transactions', 'getTransactionsByCategory')->name('budgets.getTransactionsByCategory');
 });
 
-// Pots APIs
 Route::controller(PotController::class)->name('pots.')->group(function() {
     Route::patch('/pots/{pot}/money', 'addMoney')->name('addMoney');
     Route::patch('/pots/{pot}/withdraw','withdrawMoney')->name('withdrawMoney');
 });
 
-// Balances APIs
-
-Route::controller(BalanceController::class)->group(function() {
-    // Los names evitas utilizar por todo lados las url completas, por ejemplo en ls redirects.
-    Route::get('/balances/{id}','show')->where('id', '[0-9]+')->name('balances.show');
-    Route::patch('/balances/{id}', 'update')->whereNumber('id')->name('balances.update');
-});
-
 Route::apiResource("budgets", BudgetController::class);
 Route::apiResource('pots', PotController::class);
+Route::apiResource('balances', BalanceController::class);
